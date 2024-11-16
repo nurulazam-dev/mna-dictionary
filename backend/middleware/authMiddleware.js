0cbb6ca98bb8a9348e9436b621bc7a75d071565b
mnaofficialbd <mna.official92@gmail.com>
2024-11-16 11:34:46 +0600

    authMiddleware setup

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const protect = async (req, res, next) => {
+  let token = req.cookies.token || req.headers.authorization;
+
+  if (!token) return res.status(401).json({ message: "Not authorized" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id);
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Authorization failed" });
+  }
+};
+
+const adminOnly = (req, res, next) => {
+  if (req.user.role !== "admin") {
+    return res.status(403).json({ message: "Admin access required" });
+  }
+  next();
+};
+
+module.exports = { protect, adminOnly };
