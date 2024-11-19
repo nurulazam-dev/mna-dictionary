7707b340f2cc3738f7c150229be0c9bc52336b2b
mnaofficialbd <mna.official92@gmail.com>
2024-11-19 23:27:45 +0600

    AuthContext create

--- frontend/src/utils/AdminRoute.jsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable react/prop-types */
-
-import { Navigate } from "react-router-dom";
-
-const AdminRoute = ({ children, user }) => {
-  return user?.role === "admin" ? children : <Navigate to="/" />;
-};
-
-export default AdminRoute;
