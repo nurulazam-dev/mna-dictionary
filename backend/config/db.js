12c4ec452de66f5ff3d3b4987a871e751f657500
mnaofficialbd <mna.official92@gmail.com>
2024-11-24 22:00:21 +0600

    db-connect config file setup

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+mongoose.set("strictQuery", false);
+const connectDB = async () => {
+  try {
+    // await mongoose.connect(process.env.LOCAL_DATABASE, {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB is connected");
+  } catch (err) {
+    console.log("MongoDB connection fail", err.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
