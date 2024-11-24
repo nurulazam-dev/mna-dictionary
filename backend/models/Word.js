f0506c3cfb306b36fe6219b7021325245572f4ea
mnaofficialbd <mna.official92@gmail.com>
2024-11-24 22:05:01 +0600

    word model update

--- backend/models/Word.js
+++ backend/models/Word.js
@@ -1,14 +1,17 @@
 import mongoose from "mongoose";
 
-const WordSchema = new mongoose.Schema({
-  word: { type: String, required: true, unique: true },
-  pronunciation: { type: String, required: true },
-  wordDefinition: { type: String, required: true },
-  partOfSpeech: { type: String, required: true },
-  meanings: [String],
-  synonyms: [String],
-  antonyms: [String],
-  exampleSentences: [String],
-});
+const wordSchema = new mongoose.Schema(
+  {
+    word: { type: String, required: true, unique: true },
+    pronunciation: { type: String, required: true },
+    wordDefinition: { type: String, required: true },
+    partOfSpeech: { type: String, required: true },
+    meanings: [String],
+    synonyms: [String],
+    antonyms: [String],
+    exampleSentences: [String],
+  },
+  { timestamps: true }
+);
 
-export default mongoose.model("Word", WordSchema);
+export default mongoose.model("Word", wordSchema);
