50e65d449a222ecd997ba01f79fbec158fe0a017
mnaofficialbd <mna.official92@gmail.com>
2024-11-24 22:06:08 +0600

    admin routes update

---
+++ backend/Routes/adminRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { isAuthenticated, isAdmin } from "../middlewares/authMiddleware.js";
+import { getUsers, deleteUser } from "../controllers/adminController.js";
+const router = express.Router();
+
+// Admin routes
+router.get("/users", isAuthenticated, isAdmin, getUsers);
+router.delete("/users/:id", isAuthenticated, isAdmin, deleteUser);
+
+module.exports = router;
