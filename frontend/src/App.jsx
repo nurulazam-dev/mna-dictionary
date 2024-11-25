ab3f7f78cc9a968b4a6d3d922ba2a4ea08603806
mnaofficialbd <mna.official92@gmail.com>
2024-11-25 09:51:50 +0600

    app update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Profile from "./pages/Profile";
-import WordDetails from "./pages/WordDetails";
-// import AdminDashboard from "./pages/AdminDashboard";
-import Register from "./pages/Register";
-import "./App.css";
-import NotFound from "./pages/NotFound";
-import Navbar from "./shared/Navbar";
-// import AdminRoute from "./utils/AdminRoute";
+import Layout from "./layout/Layout";
 
-function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/words/:id" element={<WordDetails />} />
-        {/* <Route
-          path="/admin"
-          element={
-            <AdminRoute user={user}>
-              <AdminDashboard />
-            </AdminRoute>
-          }
-        /> */}
-
-        {/* <Route path="/admin" element={<AdminDashboard />} /> */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
-  );
-}
+const App = () => {
+  return <Layout />;
+};
 
 export default App;
--- frontend/src/config.js
+++
@@ -1,4 +0,0 @@
-export const BASE_URL = "http://localhost:5000/api/v1";
-// export const BASE_URL ="https://mna-dictionary-backend.onrender.com/api/v1";
-
-// export const token = localStorage.getItem("token");
