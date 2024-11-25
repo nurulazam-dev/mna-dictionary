6146bb23f66a1efbe4e4957d2b2c4c508f523113
mnaofficialbd <mna.official92@gmail.com>
2024-11-25 09:52:29 +0600

    main update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import AuthProvider from "./context/AuthContext.jsx";
+import { ToastContainer } from "react-toastify";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter
+      future={{
+        v7_startTransition: true,
+      }}
+    >
+      <AuthProvider>
+        <ToastContainer
+          theme="dark"
+          position="top-right"
+          autoClose={3000}
+          closeOnClick
+          pauseOnHover={false}
+        />
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
